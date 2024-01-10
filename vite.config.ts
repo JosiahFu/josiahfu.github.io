fabd2b1570b13232149984b6a333b56c56288428
JosiahFu <josiahfu@gmail.com>
2024-01-10 11:45:33 -0800

    Prerender

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { htmlPrerender } from 'vite-plugin-html-prerender';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    htmlPrerender({
+      staticDir: import.meta.url.slice(7, -14) + 'dist',
+      routes: ['/'],
+    })
+  ],
 })
