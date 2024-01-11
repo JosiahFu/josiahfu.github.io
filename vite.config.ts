f3bd581adde13ba4aeb118eeb60dea8962dd70f2
JosiahFu <josiahfu@gmail.com>
2024-01-11 00:29:28 -0800

    Single file bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { htmlPrerender } from 'vite-plugin-html-prerender';
 import svgr from 'vite-plugin-svgr';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        htmlPrerender({
-            staticDir: import.meta.url.slice(7, -14) + 'dist',
-            routes: ['/'],
-        }),
         svgr(),
+        viteSingleFile(),
     ],
 });
